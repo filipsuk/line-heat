1baabf4bb75c941d5358cecd1089787780580671
filipsuk <filipsuk@gmail.com>
2026-01-28 20:02:59 +0100

    feat(extension): update build process

--- packages/vscode-extension/esbuild.mjs
+++ packages/vscode-extension/esbuild.mjs
@@ -1,12 +1,57 @@
-import { build } from 'esbuild';
+import { context } from 'esbuild';
 
-await build({
-	entryPoints: ['src/extension.ts'],
-	bundle: true,
-	platform: 'node',
-	format: 'cjs',
-	target: ['node16'],
-	outfile: 'out/extension.js',
-	sourcemap: true,
-	external: ['vscode'],
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildLogPlugin = {
+	name: 'esbuild-log',
+	setup(build) {
+		build.onStart(() => {
+			if (watch) {
+				console.log('[watch] build started');
+			}
+		});
+		build.onEnd((result) => {
+			for (const { text, location } of result.errors) {
+				console.error(`✘ [ERROR] ${text}`);
+				if (location == null) continue;
+				console.error(`    ${location.file}:${location.line}:${location.column}:`);
+			}
+			if (watch) {
+				console.log('[watch] build finished');
+			}
+		});
+	},
+};
+
+async function main() {
+	const ctx = await context({
+		entryPoints: ['src/extension.ts'],
+		bundle: true,
+		format: 'cjs',
+		minify: production,
+		sourcemap: !production,
+		sourcesContent: false,
+		platform: 'node',
+		outfile: 'out/extension.js',
+		external: ['vscode'],
+		logLevel: 'warning',
+		plugins: [esbuildLogPlugin],
+	});
+
+	if (watch) {
+		await ctx.watch();
+		return;
+	}
+
+	await ctx.rebuild();
+	await ctx.dispose();
+}
+
+main().catch((e) => {
+	console.error(e);
+	process.exit(1);
 });
